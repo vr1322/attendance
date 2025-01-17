0c3300de7e4aadbe9d29215876cbadef8adf4086
vr1322 <efj5178@gmail.com>
2025-01-17 17:53:42 +0530

    settings page completed

--- app/src/main/java/com/example/attendance/settings.java
+++ app/src/main/java/com/example/attendance/settings.java
@@ -1,6 +1,10 @@
 package com.example.attendance;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class settings extends AppCompatActivity {
-
+    private ImageView back;
+    private TextView settext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+      back = findViewById(R.id.back);
+      settext = findViewById(R.id.settext);
+      settext.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent intent = new Intent(settings.this, home.class);
+              startActivity(intent);
+          }
+      });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(settings.this, home.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
